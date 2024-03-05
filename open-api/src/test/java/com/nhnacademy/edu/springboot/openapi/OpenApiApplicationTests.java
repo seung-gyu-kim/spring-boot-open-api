ed27f22e4bef6044fea584f4cceae8090f2b7e48
김승규 <rlatmdrb1996@gmail.com>
2024-03-05 23:36:55 +0900

    first commit

---
+++ open-api/src/test/java/com/nhnacademy/edu/springboot/openapi/OpenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.edu.springboot.openapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
